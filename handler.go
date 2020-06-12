49a76a05f1e89719fd29bf198a1f914531e853b8
mit <mit945.mc@gmail.com>
2020-06-11 18:59:52 -0700

    frix

--- handler.go
+++ handler.go
@@ -14,11 +14,11 @@ func handler(ctx context.Context) (string, error) {
 	}
 	if os.Getenv("PHONENUMBER") == "" {
 		return "", fmt.Errorf("Please specify env variable PHONENUMBER including the country code")
-	}else{
+	}
 	err = sendSms(&sendSmsInput{
 		message:     fmt.Sprintf("The aws cost so far for today is $%s", cost),
 		phoneNumber: os.Getenv("PHONENUMBER"),
-	})}
+	})
 	return "v1", nil
 }
 
