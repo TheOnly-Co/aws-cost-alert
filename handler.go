0815dad8873beca7c37ec6b8a1aae8a47ca0c2f4
mit <mit945.mc@gmail.com>
2020-06-11 19:08:56 -0700

    comment out err

--- handler.go
+++ handler.go
@@ -15,10 +15,10 @@ func handler(ctx context.Context) (string, error) {
 	if os.Getenv("PHONENUMBER") == "" {
 		return "", fmt.Errorf("Please specify env variable PHONENUMBER including the country code")
 	}
-#	err = sendSms(&sendSmsInput{
-	#	message:     fmt.Sprintf("The aws cost so far for today is $%s", cost),
-	#	phoneNumber: os.Getenv("PHONENUMBER"),
-	# })
+//	err = sendSms(&sendSmsInput{
+//		message:     fmt.Sprintf("The aws cost so far for today is $%s", cost),
+//		phoneNumber: os.Getenv("PHONENUMBER"),
+//	})
 	return "v1", nil
 }
 
